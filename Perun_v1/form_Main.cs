7d18abe31b341bb3d78ed4ff54d1e48cd9e42934
Szymon Porwolik <szporwolik@gmail.com>
2019-02-21 00:15:39 +0100

    2nd commit (and test)

--- Perun_v1/form_Main.cs
+++ Perun_v1/form_Main.cs
@@ -10,11 +10,21 @@ using System.Windows.Forms;
 
 namespace Perun_v1
 {
-    public partial class Form1 : Form
+    public partial class form_Main : Form
     {
-        public Form1()
+        public form_Main()
         {
             InitializeComponent();
+        }
+
+        private void con_Button_DEBUG_Click(object sender, EventArgs e)
+        {
+            con_List_Received.Items.Add("test");
+        }
+
+        private void form_Main_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
