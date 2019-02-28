f383f3c056a1c8b85e93bb9f596bd4e8e5d78f23
Szymon Porwolik <szporwolik@gmail.com>
2019-02-28 23:06:38 +0100

    Added #4 - only one instance will be allowed to run

--- 02_Windows_App/Perun_v1/01_Classes/Program.cs
+++ 02_Windows_App/Perun_v1/01_Classes/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace Perun_v1
@@ -9,24 +10,39 @@ namespace Perun_v1
         /// <summary>
         /// Główny punkt wejścia dla aplikacji.
         /// </summary>
+
+        // Mutex to block multiple instances
+        static Mutex mutex = new Mutex(true, "{5a710507-92e9-4664-9b91-918b8b82f107}");
         [STAThread]
 
-    
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-
-            Application.ApplicationExit += new EventHandler(Application_ApplicationExit);
-
-            void Application_ApplicationExit(object sender, EventArgs e)
+            if (mutex.WaitOne(TimeSpan.Zero, true))
             {
-                Properties.Settings.Default.Save();
-            }
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.ApplicationExit += new EventHandler(Application_ApplicationExit);
 
-            Application.Run(new form_Main());
+                void Application_ApplicationExit(object sender, EventArgs e)
+                {
+                    Properties.Settings.Default.Save();
+                }
+                Application.Run(new form_Main());
+                mutex.ReleaseMutex();
+            }
+            else
+            {
+                // send our Win32 message to make the currently running instance
+                // jump on top of all the other windows
+                NativeMethods.PostMessage(
+                    (IntPtr)NativeMethods.HWND_BROADCAST,
+                    NativeMethods.WM_SHOWME,
+                    IntPtr.Zero,
+                    IntPtr.Zero);
+                MessageBox.Show("Only one instance is allowed.");
+            }
         }
 
-       
+
     }
 }
--- 02_Windows_App/Perun_v1/02_Forms/form_Main.cs
+++ 02_Windows_App/Perun_v1/02_Forms/form_Main.cs
@@ -544,5 +544,27 @@ namespace Perun_v1
                 }
             }
         }
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == NativeMethods.WM_SHOWME)
+            {
+                ShowMe();
+            }
+            base.WndProc(ref m);
+        }
+        private void ShowMe()
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                WindowState = FormWindowState.Normal;
+            }
+            // get our current "TopMost" value (ours will always be false though)
+            bool top = TopMost;
+            // make our form jump to the top of everything
+            TopMost = true;
+            // set it back to whatever it was
+            TopMost = top;
+        }
     }
 }
