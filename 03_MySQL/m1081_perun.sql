c4eb824c323319e41bcd2bf2f39feeb16e39ef60
VladMordock <szporwolik@gmail.com>
2019-03-11 21:16:47 +0100

    Update m1081_perun.sql
    Updated to v0.5.4

--- 03_MySQL/m1081_perun.sql
+++ 03_MySQL/m1081_perun.sql
@@ -1,5 +1,3 @@
-
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
@@ -44,8 +42,8 @@ DROP TABLE IF EXISTS `pe_DataPlayers`;
 CREATE TABLE IF NOT EXISTS `pe_DataPlayers` (
   `pe_DataPlayers_id` bigint(20) NOT NULL AUTO_INCREMENT,
   `pe_DataPlayers_ucid` varchar(150) NOT NULL,
-  `pe_DataPlayers_lastip` varchar(100) NOT NULL,
   `pe_DataPlayers_lastname` varchar(150) NOT NULL,
+  `pe_DataPlayers_lastip` varchar(100) NOT NULL,
   `pe_DataPlayers_updated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`pe_DataPlayers_id`),
   UNIQUE KEY `UNIQUE_UCID` (`pe_DataPlayers_ucid`)
@@ -64,6 +62,21 @@ CREATE TABLE IF NOT EXISTS `pe_DataRaw` (
   `pe_dataraw_payload` text NOT NULL,
   `pe_dataraw_updated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`pe_dataraw_type`,`pe_dataraw_instance`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `pe_DataTypes`
+--
+
+DROP TABLE IF EXISTS `pe_DataTypes`;
+CREATE TABLE IF NOT EXISTS `pe_DataTypes` (
+  `pe_DataTypes_id` int(11) NOT NULL AUTO_INCREMENT,
+  `pe_DataTypes_name` varchar(100) NOT NULL,
+  `pe_DataTypes_update` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`pe_DataTypes_id`),
+  UNIQUE KEY `UNIQUE_TYPE_NAME` (`pe_DataTypes_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -94,8 +107,8 @@ CREATE TABLE IF NOT EXISTS `pe_LogChat` (
 DROP TABLE IF EXISTS `pe_LogEvent`;
 CREATE TABLE IF NOT EXISTS `pe_LogEvent` (
   `pe_LogEvent_id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `pe_LogEvent_missionhash_id` bigint(20) DEFAULT NULL,
   `pe_LogEvent_datetime` datetime DEFAULT CURRENT_TIMESTAMP,
+  `pe_LogEvent_missionhash_id` bigint(20) DEFAULT NULL,
   `pe_LogEvent_type` varchar(100) NOT NULL,
   `pe_LogEvent_content` text NOT NULL,
   `pe_LogEvent_arg1` varchar(150) DEFAULT NULL,
@@ -131,13 +144,10 @@ CREATE TABLE IF NOT EXISTS `pe_LogLogins` (
 DROP TABLE IF EXISTS `pe_LogStats`;
 CREATE TABLE IF NOT EXISTS `pe_LogStats` (
   `pe_LogStats_id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `pe_LogStats_missionhash_id` bigint(20) DEFAULT NULL,
   `pe_LogStats_datetime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `pe_LogStats_missionhash_id` bigint(20) DEFAULT NULL,
   `pe_LogStats_playerid` bigint(20) DEFAULT NULL,
-  `pe_LogStats_type` varchar(50) DEFAULT NULL,
-  `pe_LogStats_debug` text,
-  `pe_LogStats_dcs` text,
-  `pe_LogStats_perun` text,
+  `pe_LogStats_typeid` int(11) DEFAULT NULL,
   `ps_kills` int(11) UNSIGNED NOT NULL DEFAULT '0',
   `ps_pvp` int(11) UNSIGNED NOT NULL DEFAULT '0',
   `ps_deaths` int(11) UNSIGNED NOT NULL DEFAULT '0',
@@ -151,6 +161,7 @@ CREATE TABLE IF NOT EXISTS `pe_LogStats` (
   `ps_kills_unarmed` int(11) UNSIGNED NOT NULL DEFAULT '0',
   `ps_kills_infantry` int(11) UNSIGNED NOT NULL DEFAULT '0',
   `ps_kills_ships` int(11) UNSIGNED NOT NULL DEFAULT '0',
+  `ps_kills_fortification` int(10) UNSIGNED NOT NULL DEFAULT '0',
   `ps_kills_other` int(11) UNSIGNED NOT NULL DEFAULT '0',
   `ps_airfield_takeoffs` int(11) UNSIGNED NOT NULL DEFAULT '0',
   `ps_airfield_landings` int(11) UNSIGNED NOT NULL DEFAULT '0',
@@ -161,8 +172,7 @@ CREATE TABLE IF NOT EXISTS `pe_LogStats` (
   `ps_other_landings` int(10) UNSIGNED NOT NULL DEFAULT '0',
   `ps_other_takeoffs` int(10) UNSIGNED NOT NULL DEFAULT '0',
   PRIMARY KEY (`pe_LogStats_id`),
-  UNIQUE KEY `UNIQUE_STATS_PER_MISSION_AND_UCID_AND_TYPE` (`pe_LogStats_playerid`,`pe_LogStats_missionhash_id`,`pe_LogStats_type`) USING BTREE,
-  KEY `pe_LogStats_type` (`pe_LogStats_type`)
+  UNIQUE KEY `UNIQUE_STATS_PER_MISSION_AND_UCID_AND_TYPE` (`pe_LogStats_playerid`,`pe_LogStats_missionhash_id`,`pe_LogStats_typeid`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 COMMIT;
 
