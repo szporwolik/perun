763e6e33146aad7dd97a2ace6d2d2d73ff0ecc17
szporowolik <szporwolik@gmail.com>
2019-10-20 13:43:44 +0200

    Code clean-up

--- 02_Windows_App/Perun_v1/01_Classes/PerunHelper.cs
+++ 02_Windows_App/Perun_v1/01_Classes/PerunHelper.cs
@@ -6,14 +6,19 @@ internal class PerunHelper
 {
     public static void LogHistoryAdd(ref string[] arrLogHistory, string strEntryToAdd)
     {
-        // Add entry to log history and rotate
+        // Rotate log history
         for (int i = 0; i < arrLogHistory.Length - 1; i++)
         {
             arrLogHistory[i] = arrLogHistory[i + 1]; // Shift one down
         }
 
+        // Add new entry
         arrLogHistory[arrLogHistory.Length - 1] = DateTime.Now.ToString("yyyy-dd-MM HH:mm:ss") + " > " + strEntryToAdd; // Add entry at the last position
+        
+        // Add the entry to log file
         LogController.WriteLog(arrLogHistory[arrLogHistory.Length - 1]);
+
+        // Update control at my window
         Globals.bLogHistoryUpdate = true;
     }
     public static string GetAppVersion(string strBeginning)
@@ -21,11 +26,13 @@ internal class PerunHelper
         // Gets build version
         if (System.Deployment.Application.ApplicationDeployment.IsNetworkDeployed)
         {
+            // For network deployed
             System.Deployment.Application.ApplicationDeployment cd = System.Deployment.Application.ApplicationDeployment.CurrentDeployment;
             Globals.strPerunVersion = cd.CurrentVersion.ToString();
         }
         else
         {
+            // For other cases
             Globals.strPerunVersion = Assembly.GetExecutingAssembly().GetName().Version.ToString();
         }
         return strBeginning+"v" + Globals.strPerunVersion;
