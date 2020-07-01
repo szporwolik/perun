4bceb1b94452d69e36cc19c049d92a76750a04fa
szporowolik <szporwolik@gmail.com>
2020-07-01 10:07:38 +0200

    Config fo 0.10.0 release

--- 03_MySQL/m1081_perun.sql
+++ 03_MySQL/m1081_perun.sql
@@ -17,7 +17,7 @@ CREATE TABLE IF NOT EXISTS `pe_Config` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 INSERT INTO `pe_Config` (`pe_Config_id`, `pe_Config_payload`) VALUES
-(1, 'v0.9.5');
+(1, 'v0.10.0');
 
 DROP TABLE IF EXISTS `pe_DataMissionHashes`;
 CREATE TABLE IF NOT EXISTS `pe_DataMissionHashes` (
